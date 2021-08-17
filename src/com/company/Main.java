51976159aa9a0c8cac750ab4197481ccffb603a8
Manny Lee <many221@gmail.com>
2021-08-17 16:37:36 -0700

    finished the remove dice method for player class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,14 +26,19 @@ public class Main {
 
         player1.rollDice ();//sets the dice in players hand
         player2.rollDice ();
-        System.out.println (player1.getName () + " --- " + player1.getDie () + " --- " + Arrays.toString ( player1.getHand () ) );
-        player1.rollDice ();
-        System.out.println (player2.getName () + " --- " + player2.getDie () + " --- " + Arrays.toString ( player2.getHand () ) );
-        System.out.println (player1.getName () + " --- " + player1.getDie () + " --- " + Arrays.toString ( player1.getHand () ) );
+        System.out.println (player1.getName () + " --- " + player1.getDie () + " --- " + player1.getHand () );
+        player1.removeDice ();
+//        player1.removeDice ();
+//        player1.removeDice ();
+//        player1.removeDice (); IT WORKS YEEEEEEAAAAHHHH
+//        player1.removeDice ();
+        System.out.println (player2.getName () + " --- " + player2.getDie () + " --- " + player2.getHand () );
+        System.out.println (player1.getName () + " --- " + player1.getDie () + " --- " + player1.getHand ());
+        System.out.println (player1.getName () + " --- " + player1.getDie () + " --- " + player1.getHand ());
 
-        for (int i = 0; i < 3; i++) {
-            player2.rollDice ();
-            System.out.println ( "--------------------\n"+player2.getName () + " --- " + player2.getDie () + " --- " + Arrays.toString ( player2.getHand () ) + "\n--------------------" );
-        }
+//        for (int i = 0; i < 3; i++) {
+//            player2.rollDice ();
+//            System.out.println ( "--------------------\n"+player2.getName () + " --- " + player2.getDie () + " --- " + Arrays.toString ( player2.getHand () ) + "\n--------------------" );
+//        }
     }
 }
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,12 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Player {
 
     private  String name;
     private  byte die; //needs to be a non-static field
-    private  byte[] hand; // Think of hand as inventory as in what's in the player's
+    private ArrayList <Byte> hand = new ArrayList<Byte> (die); // Think of hand as inventory as in what's in the player's
     // possession such as dice or scorecards or money
 
     //Will be used in table class
@@ -17,7 +18,8 @@ public class Player {
 
         this.name = name;
         this.die = dice;
-        hand = new byte[die];
+        ;
+
     }
 
 
@@ -31,23 +33,24 @@ public class Player {
         return die;
     }
 
-    public byte[] getHand() {
+    public ArrayList<Byte> getHand() {
         return hand;
     }
-
-    //this may also move to table class
-    public void rollDice(){
-
-        for (int i = 0; i < getDie() ; i++) {
-
-            getHand()[i] = Dice.roll ();
-        }
-    }
-
     public void removeDice(){
         //for now it will only remove 1 dice
         byte loss = 1;
         die -= loss;
+        getHand ().remove ( 0 );
     }
+    //this may also move to table class
+    public void rollDice(){
+
+        for (int i = 0; i < die ; i++) {
+
+            getHand ().add ( Dice.roll () );
+        }
+    }
+
+
 
 }
