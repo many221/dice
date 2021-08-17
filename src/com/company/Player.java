4e852f76ceb3441d08326a18ab683b20d58d3fa7
Manny Lee <many221@gmail.com>
2021-08-17 02:18:41 -0700

    added base for player class & notes for table class

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,8 +1,76 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Player {
 
-    public Player(){
+    private final String name;
+    private static byte die;
+
+    private static byte[] hand; // Think of hand as inventory as in what's in the player's
+    // possession such as dice or scorecards or money
+
+    //Will be used in table class
+    private byte turnPosition;
+    private int score;
+
+    public Player(String name){
+
+        this.name = name;
+
+        hand = new byte[die];
 
     }
+
+    //this may move to table class
+    public static void setDie(byte die) {
+        Player.die = die;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static byte getDie() {
+        return die;
+    }
+
+     public  static byte[] getHand() {
+        return hand;
+    }
+
+    //this may also move to table class
+    public static void rollDice(){
+
+        for (int i = 0; i < die ; i++) {
+
+            getHand()[i] = Dice.roll ();
+        }
+    }
+
+
+
+    //Testing remove a die from player class but will move to table class
+
+    //Testing if player can roll dice with each roll random
+    public static void main(String[] args) {
+        Player.setDie ( (byte) 5);
+
+        Player player1 = new Player ( "Bob");
+        Player player2 = new Player ( "Jim" );
+
+        //FIXME with this order the last value over rides the previous players roll
+//        player1.rollDice();
+//        player2.rollDice();
+
+        //This order does not
+        player1.rollDice ();
+        System.out.println (player1.name + " --- " + player1.getDie () + " --- " + Arrays.toString ( player1.getHand () ) );
+        player1.rollDice ();
+        System.out.println (player2.name + " --- " + player2.getDie () + " --- " + Arrays.toString ( player2.getHand () ) );
+        
+        //As seen here so figure out how to keep it and not reset it
+        System.out.println (player1.name + " --- " + player1.getDie () + " --- " + Arrays.toString ( player1.getHand () ) );
+    }
+
 }
--- src/com/company/Table.java
+++ src/com/company/Table.java
@@ -1,4 +1,5 @@
 package com.company;
 
+//When think of table class think of it as table rules
 public class Table {
 }
